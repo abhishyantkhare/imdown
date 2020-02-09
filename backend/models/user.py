76fe3b026b7cd04601eb095cf7e36fcca65618ca
Abhishyant Khare <Abhishyant Khare>
2020-02-08 23:39:08 -0800

    migrated user columns to non nullable

--- backend/models/user.py
+++ backend/models/user.py
@@ -4,8 +4,10 @@ from flask import jsonify
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    auth_hash = db.Column(db.String(128), index=True, unique=True)
+    username = db.Column(db.String(64), index=True,
+                         unique=True, nullable=False)
+    auth_hash = db.Column(db.String(128), index=True,
+                          unique=True, nullable=False)
 
     def __repr__(self):
         return 'User {}'.format(self.username)
