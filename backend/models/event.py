38cda942202d2fb880eaad08c9b50c1dcc6f7790
Abhishyant Khare <Abhishyant Khare>
2020-02-08 18:02:22 -0800

    created all tables in db

--- backend/models/event.py
+++ backend/models/event.py
@@ -3,11 +3,11 @@ from app import db
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(64), index=True, unique=False)
-    description = db.Column(db.String(64), index=True, unique=False)
-    start_time = db.Column(db.DateTime, index=True, unique=False)
-    end_time = db.Column(db.DateTime, index=True, unique=False)
-    address = db.Column(db.String(256), index=True, unique=False)
+    title = db.Column(db.String(64), index=False, unique=False)
+    description = db.Column(db.String(64), index=False, unique=False)
+    start_time = db.Column(db.DateTime, index=False, unique=False)
+    end_time = db.Column(db.DateTime, index=False, unique=False)
+    address = db.Column(db.String(256), index=False, unique=False)
     lat = db.Column(db.Float, index=True, unique=False)
     lng = db.Column(db.Float, index=True, unique=False)
     group_id = db.Column(db.Integer, index=True, unique=False)
--- backend/models/event_response.py
+++ backend/models/event_response.py
@@ -2,9 +2,9 @@ from app import db
 
 class EventResponse(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    event_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, primary_key=True)
-    response = db.Column(db.Boolean, primary_key=True)
+    event_id = db.Column(db.Integer,  index=True, unique=True)
+    user_id = db.Column(db.Integer,  index=True, unique=True)
+    response = db.Column(db.Boolean,  index=False, unique=True)
 
     def __repr__(self):
-      return 'EventResponse event_id: {} user_id:{} response: {}'.format(event_id, user_id, response)
+      return 'EventResponse event_id: {} user_id:{} response: {}'.format(self.event_id, self.user_id, self.response)
