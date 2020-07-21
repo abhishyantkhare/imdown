762248bb10cf8dd2ed125819faf7ad5f1ecfab54
Abhishyant Khare <abhishyant@gmail.com>
2020-07-20 23:22:52 -0700

    mobile: added independent events for each group

--- imdown-mobile/squads/squads.tsx
+++ imdown-mobile/squads/squads.tsx
@@ -1,4 +1,4 @@
-import React, {useLayoutEffect, useState} from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { View, Text, FlatList, TouchableHighlight } from "react-native";
 import Divider from "../components/divider/divider";
 import { squad_styles } from "./squads_styles";
@@ -6,10 +6,30 @@ import { Button } from "react-native";
 import AddSquadModal from "./add_squad"
 
 
+type Squad = {
+  name: string,
+  events: string[]
+}
+
+const dummySquads: Squad[] = [
+  {
+    name: "BangerBrozz",
+    events: ["Beach BBQ"]
+  },
+  {
+    name: "SEP",
+    events: ["Playland"]
+  },
+  {
+    name: "CodeBase",
+    events: ["Zoom+Drinks"]
+  }
+]
+
 const Squads = (props) => {
 
   const [addSquadModalVisble, setAddSquadModalVisble] = useState(false)
-  const [squads, setSquads] = useState(props.route.params.squads)
+  const [squads, setSquads] = useState(dummySquads)
 
   useLayoutEffect(() => {
     props.navigation.setOptions({
@@ -24,24 +44,27 @@ const Squads = (props) => {
   }, [props.navigation]);
 
   const addSquad = (squadName: string) => {
-    setSquads(squads.concat([squadName]));
+    const newSquad: Squad = {
+      name: squadName,
+      events: []
+    }
+    setSquads(squads.concat([newSquad]));
     setAddSquadModalVisble(false)
   }
 
-  const goToEvents = () => {
-    console.log("CLICKED")
+  const goToEvents = (events: string[]) => {
     props.navigation.navigate("Events", {
-      events: ["Beach BBQ", "Playland", "Zoom+Drinks"]
+      events: events
     })
   }
 
 
-  const renderSquadItem = ({ item, index, separators }) => {
+  const renderSquadItem = ({ item }: { item: Squad }) => {
     return (
       <View style={squad_styles.squad_item}>
-      <TouchableHighlight onPress={goToEvents}>
-        <Text>{item}</Text>
-      </TouchableHighlight>
+        <TouchableHighlight onPress={() => { goToEvents(item.events) }}>
+          <Text>{item.name}</Text>
+        </TouchableHighlight>
       </View>
     );
   };
