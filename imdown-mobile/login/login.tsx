8ddac51201c4dc6698c5e2a4e8606a740b43a69c
Abhishyant Khare <abhishyant@gmail.com>
2020-07-22 21:27:49 -0700

    mobile: added google sign in
    Added google sign in to the app and set up a Google Cloud project
    for imdown as well. Right now the hostname in the Google Cloud project
    is Expo's hostname, when we build a standalone app we'll have to change it.

--- imdown-mobile/login/login.tsx
+++ imdown-mobile/login/login.tsx
@@ -1,19 +1,42 @@
 import React from "react";
 import { View, TextInput, Button } from "react-native";
 import { login_styles } from "./login_styles";
+import * as Google from 'expo-google-app-auth';
 
-const Login = ({navigation}) => {
+
+
+const Login = ({ navigation }) => {
+  const IOS_CLIENT_ID = '1097983281822-8qr8vltrud1hj3rfme2khn1lmbj2s522.apps.googleusercontent.com'
+  const LOGIN_SUCCESS = "success"
+
+
   const goToSquads = () => {
     navigation.navigate("Squads", {
       squads: ["SEP", "CodeBase", "BangerBrozz"]
     });
   };
 
+
+
+  const onPress = () => {
+    Google.logInAsync({
+      iosClientId: IOS_CLIENT_ID,
+    }).then((resp: Google.LogInResult) => {
+      if (resp.type === LOGIN_SUCCESS) {
+        goToSquads()
+      }
+    });
+  };
+
+
   return (
     <View style={login_styles.login_container}>
       <TextInput placeholder="Please sign in" style={login_styles.text_input} />
       <View style={login_styles.button}>
-        <Button title="Sign In" onPress={goToSquads} />
+        <Button
+          title={"sign in"}
+          onPress={onPress}
+        />
       </View>
     </View>
   );
