01e4058a79c2d9bd52eb55c5f6b0525fd9e41d8d
Vivek Jain <vivekjain03@gmail.com>
2020-07-22 21:11:14 -0400

    Emoji picker works, right color scheme and font sizes added

--- imdown-mobile/squads/add_squad.tsx
+++ imdown-mobile/squads/add_squad.tsx
@@ -1,15 +1,57 @@
 import React, {useState } from "react";
 import { View, Modal, TextInput, TouchableHighlight, Text } from "react-native";
 import {AddSquadStyles} from "./add_squad_styles"
+import EmojiSelector, { Categories } from "react-native-emoji-selector";
+import { Event } from "./events"
+
 
 type OwnProps = {
   visible: boolean,
   onPress: (squadName: string) => void
+  // onPress: (event: Event) => void
 }
 
 const AddSquadModal = (props: OwnProps) => {
     const [squadName, setSquadName] = useState("")
+    const [squadEmoji, setSquadEmoji] = useState(false)
+    const [showEmojiPicker, setShowEmojiPicker] = useState();
+    const [emojiPicked, setEmojiPicked] = useState();  
 
+    // const addSquad = () => {
+    //   const event: Event = {
+    //     name: squadName,
+    //   }
+    //   props.onPress(event)
+    // }
+
+    const renderEmoji = () => {
+      return (
+        <View >
+          <TouchableHighlight onPress={() => { setShowEmojiPicker(true) }} style={AddSquadStyles.add_squad_emoji}>
+            <Text style={AddSquadStyles.emoji}>
+             {`${emojiPicked ? squadEmoji : "TBD"}`}
+            </Text>
+          </TouchableHighlight>
+          {renderEmojiPicker()}
+         </View>
+      )
+    }
+
+    const renderEmojiPicker = () => {
+      return (
+        showEmojiPicker &&
+        <EmojiSelector
+          category={Categories.symbols}
+          onEmojiSelected={emoji => 
+            {
+              console.log(emoji)
+              setSquadEmoji(emoji);
+              setShowEmojiPicker(false);
+              setEmojiPicked(true);
+            }}
+        />
+      )
+    }
 
     return (
         <Modal
@@ -17,10 +59,14 @@ const AddSquadModal = (props: OwnProps) => {
           visible={props.visible}
           presentationStyle={"formSheet"}
         >
-          <View style={AddSquadStyles.container}>
-            <TextInput placeholder={"Add squad name"} onChangeText={(name) => setSquadName(name)}/>
+          <View style={AddSquadStyles.nameEmojiContainer}>
+            {renderEmoji()}
+            <TextInput placeholder={"add squad name"} onChangeText={(name) => setSquadName(name)} 
+              style={AddSquadStyles.squad_name}
+              placeholderTextColor = "lightgray"/>
+
             <TouchableHighlight onPress={() => props.onPress(squadName)} style={AddSquadStyles.add_squad_button}>
-              <Text>Add Squad</Text>
+              <Text style={AddSquadStyles.add_squad_text}>Add Squad</Text>
             </TouchableHighlight>
           </View>
         </Modal>
--- imdown-mobile/squads/add_squad_styles.tsx
+++ imdown-mobile/squads/add_squad_styles.tsx
@@ -1,13 +1,50 @@
 import { StyleSheet } from "react-native";
 export const AddSquadStyles = StyleSheet.create({
-  container: {
+  nameEmojiContainer: {
     flex: 1,
     flexDirection: "column",
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    // justifyContent: "center",
+    paddingTop: 40
+    
+  },
+  emoji: {
+    fontSize: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    width: 45,
+    height: 45,
+    borderWidth: 2,
+    borderRadius: 12,
+    borderColor: "#90BEDE"
   },
   add_squad_button: {
-    paddingTop: 15
+    // paddingTop: 15,
+    backgroundColor: "#90BEDE",
+    borderWidth: 10,
+    borderRadius: 20,
+    borderColor: "#90BEDE",
+    padding: 5
+  },
+  add_squad_text: {
+    fontFamily: "Ubuntu_700Bold",
+    color: "white",
+    fontSize: 18
+  },
+  add_squad_emoji: {
+    // paddingTop: 15,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  squad_name: {
+    margin: 15,
+    height: 40,
+    borderColor: "white",
+    borderBottomColor: "#90BEDE",
+    borderWidth: 1,
+    color: "#90BEDE",
+    fontFamily: "Ubuntu_700Bold",
+    fontSize: 32
   }
 });
