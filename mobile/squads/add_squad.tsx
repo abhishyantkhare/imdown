7433c9f499b40778f77079db52951fa6ef58436e
Abhishyant Khare <abhishyant@gmail.com>
2020-07-28 22:12:16 -0700

    mobile: go back to squads page from add squad

--- mobile/squads/add_squad.tsx
+++ mobile/squads/add_squad.tsx
@@ -1,14 +1,14 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import { View, Modal, TextInput, TouchableHighlight, Text, TouchableOpacity, SafeAreaView } from "react-native";
-import {AddSquadStyles} from "./add_squad_styles"
+import { AddSquadStyles } from "./add_squad_styles"
 import EmojiSelector, { Categories } from "react-native-emoji-selector";
 import { Squad } from "./squads"
 import { BACKEND_URL, callBackend } from "../backend/backend"
 
 type OwnProps = {
-  visible: boolean,
-  email: string,
-  onPress: (squad: Squad) => void
+    visible: boolean,
+    email: string,
+    onPress: (squad: Squad) => void
 }
 
 const DEFAULT_EMOJI = "😎"
@@ -20,84 +20,81 @@ const AddSquadModal = (props: OwnProps) => {
     const [emojiPicked, setEmojiPicked] = useState(DEFAULT_EMOJI);
 
     const addSquadOnBackend = () => {
-      const endpoint = 'create_squad'
-      const data = {
-        email: props.email,
-        squad_name: squadName,
-        squad_emoji: emojiPicked
-      }
-      const init: RequestInit = {
-        method: 'POST',
-        mode: 'no-cors',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      }
-      callBackend(endpoint, init).then((resp: Response) => {
-        resp.text()
-      }).then(result => { addSquad() })
+        const endpoint = 'create_squad'
+        const data = {
+            email: props.email,
+            squad_name: squadName,
+            squad_emoji: emojiPicked
+        }
+        const init: RequestInit = {
+            method: 'POST',
+            mode: 'no-cors',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }
+        callBackend(endpoint, init).then(() => { addSquad() })
     }
-    
+
     const addSquad = () => {
-      const squad: Squad = {
-        name: squadName,
-        emoji: emojiPicked,
-        email: props.email,
-        events: []
-      }
-      props.onPress(squad)
+        const squad: Squad = {
+            name: squadName,
+            emoji: emojiPicked,
+            email: props.email,
+            events: []
+        }
+        props.onPress(squad)
     }
 
     const renderEmoji = () => {
-      return (
-        <View >
-          <TouchableOpacity onPress={() => { setShowEmojiPicker(true) }} style={AddSquadStyles.squad_emoji}>
-            <Text style={AddSquadStyles.emoji}>
-             {`${showSquadEmoji ? emojiPicked : DEFAULT_EMOJI }`}
-            </Text>
-          </TouchableOpacity>
-          <View style={AddSquadStyles.squad_emoji}>
-            {renderEmojiPicker()}
-          </View>
-         </View>
-      )
+        return (
+            <View >
+                <TouchableOpacity onPress={() => { setShowEmojiPicker(true) }} style={AddSquadStyles.squad_emoji}>
+                    <Text style={AddSquadStyles.emoji}>
+                        {`${showSquadEmoji ? emojiPicked : DEFAULT_EMOJI}`}
+                    </Text>
+                </TouchableOpacity>
+                <View style={AddSquadStyles.squad_emoji}>
+                    {renderEmojiPicker()}
+                </View>
+            </View>
+        )
     }
 
     const renderEmojiPicker = () => {
-      return (
-        showEmojiPicker &&
-        <EmojiSelector
-          category={Categories.symbols}
-          onEmojiSelected={emoji => 
-            {
-              setEmojiPicked(emoji);
-              setShowEmojiPicker(false);
-              setShowSquadEmoji(true);
-            }}
-        />
-      )
+        return (
+            showEmojiPicker &&
+            <EmojiSelector
+                category={Categories.symbols}
+                onEmojiSelected={emoji => {
+                    setEmojiPicked(emoji);
+                    setShowEmojiPicker(false);
+                    setShowSquadEmoji(true);
+                }}
+            />
+        )
     }
 
     return (
         <Modal
-          transparent={false}
-          visible={props.visible}
-          presentationStyle={"formSheet"}
+            transparent={false}
+            visible={props.visible}
+            presentationStyle={"formSheet"}
         >
-          <View style={AddSquadStyles.container}>
-            <SafeAreaView style={AddSquadStyles.emoji_and_squad_name_container}>
-              {renderEmoji()}
-              <TextInput placeholder={"add squad name"} onChangeText={(name) => setSquadName(name)} 
-                style={AddSquadStyles.squad_name}
-                placeholderTextColor = "lightgray"/>
-            </SafeAreaView>
-            
-            <TouchableOpacity onPress={addSquadOnBackend} style={AddSquadStyles.add_squad_button}>
-              <Text style={AddSquadStyles.add_squad_text}>Done</Text>
-            </TouchableOpacity>
+            <View style={AddSquadStyles.container}>
+                <SafeAreaView style={AddSquadStyles.emoji_and_squad_name_container}>
+                    {renderEmoji()}
+                    <TextInput placeholder={"add squad name"} onChangeText={(name) => setSquadName(name)}
+                        style={AddSquadStyles.squad_name}
+                        placeholderTextColor="lightgray" />
+                </SafeAreaView>
 
-          </View>
+                <TouchableOpacity onPress={addSquadOnBackend} style={AddSquadStyles.add_squad_button}>
+                    <Text style={AddSquadStyles.add_squad_text}>Done</Text>
+                </TouchableOpacity>
+
+            </View>
 
         </Modal>
     );
