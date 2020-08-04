b3e09046c1e171184a559743415d9473f8ad9c61
Vivek Jain <vivekjain03@gmail.com>
2020-08-04 18:34:18 -0400

    Added functionality to delete a user from squad

--- mobile/squads/squad_members.tsx
+++ mobile/squads/squad_members.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, FlatList, Image} from "react-native";
+import { View, Text, FlatList, Image, Alert} from "react-native";
 import { squad_members_styles } from "./squad_members_styles";
-import {callBackend } from "../backend/backend"
-import {User} from "../types/user"
+import { callBackend } from "../backend/backend"
+import { User } from "../types/user"
+import Swipeout from 'react-native-swipeout';
 
 const SquadMembers = (props) => {
     const [users, setUsers] = useState([])
@@ -23,16 +24,71 @@ const SquadMembers = (props) => {
         });
     }, []);  
 
-    const renderUsersItem = ({ item }: { item: User }) => {
+    const deleteUser = (userId: number, index: number) => {
+        const endpoint = 'delete_user'
+        const data = {
+            user_id: userId,
+            squad_id: squadId
+        }
+        const init: RequestInit = {
+            method: 'DELETE',
+            mode: 'no-cors',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }
+        callBackend(endpoint, init).then(response => { 
+            return response.json();
+        }).then(data => { 
+            setUsers(data.user_info);
+        });
+    }
+
+    const deletebtn = (id: number, name: string, index: number) => {
         return (
-          <View style={squad_members_styles.user_info_view}>
-            <View style={{paddingRight: 10}}>
-                <Image style={squad_members_styles.user_image} source={{ uri: item.photo }} />
-            </View>
-            <View style={{paddingBottom: 8}}>
-                <Text style={squad_members_styles.user_text}>{item.name} </Text>
-            </View>
-          </View>
+            [
+                {
+                  text: 'delete', 
+                  onPress: () => { 
+                    Alert.alert(
+                       'Alert',
+                       'Are you sure you want to delete ' + name   + '?',
+                       [
+                            {
+                                text: 'Yes', 
+                                onPress: () => { deleteUser(id, index) },
+                            },
+                            {
+                                text: 'Cancel', 
+                                style: "cancel"
+                            }
+                       ],
+                       { cancelable: true}
+                    )   
+                  },
+                  backgroundColor: "red"
+                }
+            ]
+        )
+    } 
+    
+
+    const renderUsersItem = ({ item, index }: { item: User, index: number }) => {
+        return (
+          <Swipeout right={deletebtn(item.id, item.name, index)} 
+                    backgroundColor="white"
+                    autoClose={true}
+                    >
+                <View style={squad_members_styles.user_info_view}>
+                    <View style={{paddingRight: 10}}>
+                        <Image style={squad_members_styles.user_image} source={{ uri: item.photo }} />
+                    </View>
+                    <View style={{paddingBottom: 8}}>
+                        <Text style={squad_members_styles.user_text}>{item.name} </Text>
+                    </View>
+                </View>
+          </Swipeout>  
         );
       };
 
