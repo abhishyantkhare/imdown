1c2992bdab6da7672346925c0576dcb804dbbc60
Abhishyant Khare <abhishyant@gmail.com>
2020-08-05 20:19:55 -0700

    mobile: send google server code to backend after sign in
    Send the Google server code for the user to the backend so that we can
    auth the user on our server

--- mobile/login/login.tsx
+++ mobile/login/login.tsx
@@ -9,85 +9,89 @@ import { User } from "../types/user"
 
 
 const Login = ({ navigation }) => {
-  const [isSigninInProgress, setIsSigninInProgress] = useState(false);
+    const [isSigninInProgress, setIsSigninInProgress] = useState(false);
+    const IOS_CLIENT_ID = "1097983281822-8qr8vltrud1hj3rfme2khn1lmbj2s522.apps.googleusercontent.com";
+    const WEB_ID = "1097983281822-8k2kede3hrgrqi15r869mf3u5at6q6ib.apps.googleusercontent.com"
 
-  const IOS_CLIENT_ID = "1097983281822-8qr8vltrud1hj3rfme2khn1lmbj2s522.apps.googleusercontent.com";
 
-  const goToSquads = (email: string) => {
-    navigation.navigate("Squads", {
-      squads: [],
-      email: email
+    const goToSquads = (email: string) => {
+        navigation.navigate("Squads", {
+            squads: [],
+            email: email
+        });
+    };
+
+    const signInOnBackend = (user: User, googleServerCode: string) => {
+        const login_url = BACKEND_URL + 'sign_in'
+        const data = {
+            email: user.email,
+            name: user.name,
+            photo: user.photo,
+            googleServerCode: googleServerCode
+        }
+        fetch(login_url, {
+            method: 'POST',
+            mode: 'no-cors',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then((resp: Response) => {
+            setCookieAndTransition(resp.headers.get("set-cookie"), user.email)
+        })
+    }
+
+    const setCookieAndTransition = (sessionCookie: string, email: string) => {
+        AsyncStorage.setItem("sessionCookie", sessionCookie).then(() => {
+            goToSquads(email);
+        })
+    }
+
+
+    const signIn = async () => {
+        try {
+            setIsSigninInProgress(true);
+            await GoogleSignin.hasPlayServices();
+            const resp = await GoogleSignin.signIn();
+            const user: User = {
+                email: resp.user.email,
+                name: resp.user.name,
+                photo: resp.user.photo
+            };
+            setIsSigninInProgress(false);
+            signInOnBackend(user, resp.serverAuthCode);
+        } catch (error) {
+            // TODO: catch error codes here.
+        }
+    };
+
+
+    GoogleSignin.configure({
+        iosClientId: IOS_CLIENT_ID,
+        webClientId: WEB_ID,
+        offlineAccess: true
     });
-  };
 
 
-  const signInOnBackend = (user: User) => {
-    const login_url = BACKEND_URL + 'sign_in'
-    const data = {
-      email: user.email,
-      name: user.name,
-      photo: user.photo
-    }
-    fetch(login_url, {
-      method: 'POST',
-      mode: 'no-cors',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then((resp: Response) => {
-      setCookieAndTransition(resp.headers.get("set-cookie"), user.email)
-    })
-  }
-
-  const setCookieAndTransition = (sessionCookie: string, email: string) => {
-    AsyncStorage.setItem("sessionCookie", sessionCookie).then(() => {
-      goToSquads(email);
-    })
-  }
-
-
-  const signIn = async () => {
-    try {
-      setIsSigninInProgress(true);
-      await GoogleSignin.hasPlayServices();
-      const resp = await GoogleSignin.signIn();
-      const user: User = {
-        email: resp.user.email,
-        name: resp.user.name,
-        photo: resp.user.photo
-      };
-      setIsSigninInProgress(false);
-      signInOnBackend(user);
-    } catch (error) {
-      // TODO: catch error codes here.
-    }
-  };
-
-  GoogleSignin.configure({
-    iosClientId: IOS_CLIENT_ID
-  });
-
-
-  return (
-    <View style={login_styles.login_container}>
-      <LinearGradient
-        colors={["#90BEDE", "#C7F9FF"]}
-        style={login_styles.gradient_background}
-      />
-      <View style={login_styles.title_description_container}>
-        <Text style={login_styles.imdown_title}>imdown</Text>
-        <Text style={login_styles.imdown_description}>A better way to manage group events.</Text>
-      </View>
-      <View style={login_styles.google_sign_in_button_container}>
-        <GoogleSigninButton
-          style={login_styles.google_sign_in_button}
-          size={GoogleSigninButton.Size.Wide}
-          onPress={signIn}
-          disabled={isSigninInProgress} />
-      </View>
-    </View>
-  );
+    return (
+        <View style={login_styles.login_container}>
+            <LinearGradient
+                colors={["#90BEDE", "#C7F9FF"]}
+                style={login_styles.gradient_background}
+            />
+            <View style={login_styles.title_description_container}>
+                <Text style={login_styles.imdown_title}>imdown</Text>
+                <Text style={login_styles.imdown_description}>A better way to manage group events.</Text>
+            </View>
+            <View style={login_styles.google_sign_in_button_container}>
+                <GoogleSigninButton
+                    style={login_styles.google_sign_in_button}
+                    size={GoogleSigninButton.Size.Wide}
+                    onPress={signIn}
+                    disabled={isSigninInProgress} />
+            </View>
+        </View>
+    );
 };
 
 export default Login;
