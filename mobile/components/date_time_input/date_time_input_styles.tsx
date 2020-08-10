97695815a52ea0ee0e80c46a0a3dce862015766e
c0dyzeng <codyzeng@berkeley.edu>
2020-08-09 17:40:15 -0700

    Add styles for date time picker

---
+++ mobile/components/date_time_input/date_time_input_styles.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+export const DateTimeInputStyles = StyleSheet.create({
+  time_text: {
+    color: "black",
+    fontFamily: "Ubuntu_400Regular",
+    fontSize: 15,
+    paddingLeft: 10,
+    paddingVertical: 15,
+  },
+});
+
