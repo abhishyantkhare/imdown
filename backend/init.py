9d2e0b76501cf2e0be9860ec64f4fee68cc282b3
Abhishyant Khare <abhishyant@gmail.com>
2020-08-09 20:23:04 -0700

    backend: load flask secret key from AWS for prod

--- backend/init.py
+++ backend/init.py
@@ -16,9 +16,21 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
+def gen_secret_key(application, secrets_manager):
+    if "FLASK_ENV" in application.config and application.config["FLASK_ENV"] == "development":
+        return os.urandom(16)
+    key_json = secrets_manager.get_secret_value(
+        SecretId="flask_secret_key")["SecretString"]
+    key = json.loads(key_json)["flask_secret_key"]
+    return key
+
+
 application = Flask(__name__)
-application.secret_key = os.urandom(16)
 application.config.from_object(Config)
+secrets_manager = boto3.client('secretsmanager')
+
+application.secret_key = gen_secret_key(application, secrets_manager)
+
 
 login_manager.init_app(application)
 db.init_app(application)
@@ -27,7 +39,6 @@ migrate.init_app(application, db)
 
 def read_secrets():
     secrets = dict()
-    secrets_manager = boto3.client('secretsmanager')
     google_secrets_json = secrets_manager.get_secret_value(
         SecretId='google_client')['SecretString']
 
