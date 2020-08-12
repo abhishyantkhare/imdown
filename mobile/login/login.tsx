5c428a544598207d6f6723d2a56cc1a508d0d5a7
c0dyzeng <codyzeng@berkeley.edu>
2020-08-11 21:27:06 -0700

    Catch common errors

--- mobile/login/login.tsx
+++ mobile/login/login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Text, View } from "react-native";
 import { login_styles } from "./login_styles";
-import { GoogleSignin, GoogleSigninButton } from '@react-native-community/google-signin';
+import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-community/google-signin';
 import { LinearGradient } from 'expo-linear-gradient';
 import AsyncStorage from '@react-native-community/async-storage';
 import { BACKEND_URL } from "../backend/backend"
@@ -54,16 +54,26 @@ const Login = ({ navigation }) => {
           await GoogleSignin.hasPlayServices();
           const resp = await GoogleSignin.signIn();
           const user: User = {
-              email: resp.user.email,
-              name: resp.user.name,
-              photo: resp.user.photo
+            email: resp.user.email,
+            name: resp.user.name,
+            photo: resp.user.photo
           };
           setIsSigninInProgress(false);
           signInOnBackend(user, resp.serverAuthCode);
         } catch (error) {
           setIsSigninInProgress(false);
-          console.log("Error signing into Google account. Error is " + error)
-            // TODO: catch error codes here.
+          if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+            // user cancelled the login flow
+            console.log("Error signing into Google account. User cancelled the login flow. Error is " + error);
+          } else if (error.code === statusCodes.IN_PROGRESS) {
+            // operation (e.g. sign in) is in progress already
+            console.log("Error signing into Google account. User is already in the process of logging in. Error is " + error);
+          } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+            // play services not available or outdated
+            console.log("Error signing into Google account. Play services are not available. Error is " + error);
+          } else {
+            console.log("Error signing into Google account. Unidentified error. It is " + error);
+          }
         }
     };
 
