2cd836dd02a0bf8cbc22e619caf9f329e211d9d3
Brian Levis <brianlevis1@gmail.com>
2020-08-30 21:48:21 -0700

    Remove redundant helper function for models

--- backend/models/squadmembership.py
+++ backend/models/squadmembership.py
@@ -1,6 +1,6 @@
 from init import db
 from flask import jsonify
-from models.user import User, get_user_by_id
+from models.user import User
 
 
 class SquadMembership(db.Model):
@@ -21,6 +21,6 @@ def GetUsersBySquadId(squad_id):
     users_lst = []
     for user_squad_membership in user_squad_memberships:
         user_id = user_squad_membership.user_id
-        user = get_user_by_id(user_id)
+        user = User.query.get(user_id)
         users_lst.append(user)
     return [user.userDict() for user in users_lst]
