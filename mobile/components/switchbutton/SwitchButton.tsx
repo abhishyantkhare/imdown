9691d66dc8d9ad82875133924c86ef7e7c98c1a9
Cynthia Wang <cynthiawang@berkeley.edu>
2020-09-13 15:10:39 +0800

    switch button linting

--- mobile/components/switchbutton/SwitchButton.tsx
+++ mobile/components/switchbutton/SwitchButton.tsx
@@ -1,30 +1,43 @@
-import React, { useState } from "react"
-import { View, Switch } from "react-native"
-import Label from "../label/Label"
-import SwitchButtonStyles from "./SwitchButtonStyles"
+import React, { useState } from 'react';
+import { View, Switch } from 'react-native';
+
+import Label from '../label/Label';
+import SwitchButtonStyles from './SwitchButtonStyles';
 
 type SwitchButtonProps = {
-    label: string,
-    onChange: () => void,
-    initialValue?: boolean,
-    style?: object
-}
+  label: string;
+  onChange: () => void;
+  initialValue?: boolean;
+  style?: object;
+};
 
 const SwitchButton = (props: SwitchButtonProps) => {
-    const [value, setValue] = useState(props.initialValue || false)
-    return (
-        <View style={[SwitchButtonStyles.switchButtonContainer, props.style]}>
-            <Label labelText={props.label} size={"small"} />
-            <Switch
-                style={{ right: 0, position: "absolute" }}
-                onValueChange={(val) => {
-                    setValue(val)
-                    props.onChange()
-                }}
-                value={value}
-            />
-        </View>
-    )
-}
+  const {
+    initialValue,
+    label,
+    style,
+    onChange,
+  } = props;
+  const [value, setValue] = useState(initialValue || false);
 
-export default SwitchButton
+  return (
+    <View style={[SwitchButtonStyles.switchButtonContainer, style]}>
+      <Label labelText={label} size='small' />
+      <Switch
+        style={{ right: 0, position: 'absolute' }}
+        onValueChange={(val) => {
+          setValue(val);
+          onChange();
+        }}
+        value={value}
+      />
+    </View>
+  );
+};
+
+SwitchButton.defaultProps = {
+  initialValue: false,
+  style: {},
+};
+
+export default SwitchButton;
