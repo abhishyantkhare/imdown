4ec37e1a6adb38f4f2a6ea4699ff9761e3002d69
Cynthia Wang <cynthiawang@berkeley.edu>
2020-09-13 15:07:26 +0800

    section linting

--- mobile/components/section/SectionStyles.tsx
+++ mobile/components/section/SectionStyles.tsx
@@ -1,13 +1,13 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet } from 'react-native';
 
 const SectionStyles = StyleSheet.create({
-    labelHeader: {
-        display: "flex",
-        flexDirection: "row",
-        marginTop: "7.5%",
-        marginBottom: "5%",
-        alignItems: "baseline"
-    }
-})
+  labelHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: '7.5%',
+    marginBottom: '5%',
+    alignItems: 'baseline',
+  },
+});
 
-export default SectionStyles
+export default SectionStyles;
