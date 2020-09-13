4810529adc74b252e9532e41438ab401aa18b49f
Cynthia Wang <cynthiawang@berkeley.edu>
2020-09-13 19:20:43 +0800

    textstyles linting

--- mobile/TextStyles.tsx
+++ mobile/TextStyles.tsx
@@ -1,23 +1,26 @@
-import { StyleSheet } from "react-native";
-export const TextStyles = StyleSheet.create({
+import { StyleSheet } from 'react-native';
+
+const TextStyles = StyleSheet.create({
   title: {
-    fontFamily: "SourceSansPro_700Bold",
-    fontSize: 40
+    fontFamily: 'SourceSansPro_700Bold',
+    fontSize: 40,
   },
   headerLarge: {
-    fontFamily: "SourceSansPro_700Bold",
-    fontSize: 30
+    fontFamily: 'SourceSansPro_700Bold',
+    fontSize: 30,
   },
   headerSmall: {
-    fontFamily: "SourceSansPro_700Bold",
-    fontSize: 20
+    fontFamily: 'SourceSansPro_700Bold',
+    fontSize: 20,
   },
   paragraph: {
-    fontFamily: "Roboto_400Regular",
-    fontSize: 20
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 20,
   },
   secondary: {
-    fontFamily: "Roboto_400Regular",
-    fontSize: 15
-  }
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 15,
+  },
 });
+
+export default TextStyles;
