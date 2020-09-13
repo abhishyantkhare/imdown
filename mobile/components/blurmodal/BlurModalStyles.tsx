cd71d821eb706c842929b6189828fdc7954b4697
Cynthia Wang <cynthiawang@berkeley.edu>
2020-09-13 14:30:58 +0800

    blurmodal linting

--- mobile/components/blurmodal/BlurModalStyles.tsx
+++ mobile/components/blurmodal/BlurModalStyles.tsx
@@ -1,29 +1,29 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet } from 'react-native';
 
 const BlurModalStyles = StyleSheet.create({
-    modalBackgroundBlur: {
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: 'rgba(100,100,100, 0.5)',
-        flex: 1,
-    },
-    modalVisibleContainer: {
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "white",
-        borderRadius: 10,
-        width: "80%"
-    },
-    exitButtonContainer: {
-        marginBottom: 25,
-        marginTop: 25,
-        marginHorizontal: 20,
-    },
-    exitButton: {
-        alignSelf: 'flex-end',
-        height: 40,
-        width: 40,
-    },
-})
+  modalBackgroundBlur: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(100,100,100, 0.5)',
+    flex: 1,
+  },
+  modalVisibleContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    width: '80%',
+  },
+  exitButtonContainer: {
+    marginBottom: 25,
+    marginTop: 25,
+    marginHorizontal: 20,
+  },
+  exitButton: {
+    alignSelf: 'flex-end',
+    height: 40,
+    width: 40,
+  },
+});
 
-export default BlurModalStyles
+export default BlurModalStyles;
