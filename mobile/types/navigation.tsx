c06ece8fb415315dea95967bb3ed228e538faca9
Cynthia Wang <cynthiawang@berkeley.edu>
2020-09-13 19:24:11 +0800

    navigation type linting

--- mobile/types/navigation.tsx
+++ mobile/types/navigation.tsx
@@ -1,16 +1,17 @@
-import { RouteProp } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
-import { RootStackParamList } from "../App";
+import { RouteProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+import { RootStackParamList } from '../App';
 
 type AppNavigationProp<K extends keyof RootStackParamList> = StackNavigationProp<
-    RootStackParamList,
-    K>;
+  RootStackParamList,
+  K>;
 
 type AppRouteProp<K extends keyof RootStackParamList> = RouteProp<RootStackParamList, K>;
 
 type AppNavRouteProp<K extends keyof RootStackParamList> = {
-    navigation: AppNavigationProp<K>;
-    route: AppRouteProp<K>
+  navigation: AppNavigationProp<K>;
+  route: AppRouteProp<K>;
 };
 
-export default AppNavRouteProp
+export default AppNavRouteProp;
