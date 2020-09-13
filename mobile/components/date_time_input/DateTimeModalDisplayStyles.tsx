ca3ff35d4b40d528b680a847b8d7b169c5979872
Cynthia Wang <cynthiawang@berkeley.edu>
2020-09-13 15:26:32 +0800

    datetime modal display linting

--- mobile/components/date_time_input/DateTimeModalDisplayStyles.tsx
+++ mobile/components/date_time_input/DateTimeModalDisplayStyles.tsx
@@ -1,26 +1,26 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet } from 'react-native';
 
 const DateTimeModalDisplayStyles = StyleSheet.create({
-    displayContainer: {
-        backgroundColor: "#E7EAF6",
-        padding: 15,
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: 5
-    },
-    dateTimeModalStyle: {
-        marginTop: "auto",
-        marginBottom: "auto",
-        width: "80%",
-        marginLeft: "auto",
-        marginRight: "auto",
-        padding: 20,
-        backgroundColor: "#FFF"
-    },
-    actionButton: {
-        marginHorizontal: 10,
-        width: "50%"
-    }
-})
+  displayContainer: {
+    backgroundColor: '#E7EAF6',
+    padding: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  dateTimeModalStyle: {
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    width: '80%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    padding: 20,
+    backgroundColor: '#FFF',
+  },
+  actionButton: {
+    marginHorizontal: 10,
+    width: '50%',
+  },
+});
 
-export default DateTimeModalDisplayStyles
+export default DateTimeModalDisplayStyles;
