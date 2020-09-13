6cf91ef762c9b9a66f9a36aa62a8d4a9dc2ef026
Cynthia Wang <cynthiawang@berkeley.edu>
2020-09-13 14:47:17 +0800

    emojipicker linting

--- mobile/components/emojipicker/EmojiPicker.tsx
+++ mobile/components/emojipicker/EmojiPicker.tsx
@@ -1,55 +1,67 @@
-import React, { useState } from "react";
-import { Modal, TouchableOpacity, Button, Text, View, SafeAreaView } from "react-native";
-import EmojiSelector from "react-native-emoji-selector";
-import { EmojiPickerStyles } from "./EmojiPickerStyles"
-import { DEFAULT_EMOJI } from "../../constants"
+import React, { useState } from 'react';
+import {
+  Modal,
+  TouchableOpacity,
+  Button,
+  Text,
+  View,
+  SafeAreaView,
+} from 'react-native';
+import EmojiSelector from 'react-native-emoji-selector';
+
+import EmojiPickerStyles from './EmojiPickerStyles';
+import { DEFAULT_EMOJI } from '../../constants';
 
 type EmojiPickerProps = {
-    defaultEmoji?: string
-    onEmojiPicked: (emoji: string) => void
-    emojiPickerTitle: string
+    defaultEmoji?: string,
+    onEmojiPicked: (emoji: string) => void, // eslint-disable-line no-unused-vars
+    emojiPickerTitle: string,
 }
 
 const EmojiPicker = (props: EmojiPickerProps) => {
-    const [showEmojiPicker, setShowEmojiPicker] = useState(false)
-    const defaultEmoji = props.defaultEmoji || DEFAULT_EMOJI
-    const [emojiPicked, setEmojiPicked] = useState(defaultEmoji)
+  const { emojiPickerTitle, defaultEmoji } = props;
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [emojiPicked, setEmojiPicked] = useState(defaultEmoji);
 
-    return showEmojiPicker ? (
-        <Modal visible={showEmojiPicker}>
-            <SafeAreaView style={EmojiPickerStyles.modalContainer}>
-                <View style={EmojiPickerStyles.headerContainer}>
-                    <View style={EmojiPickerStyles.buttonContainer}>
-
-                    </View>
-                    <View style={EmojiPickerStyles.titleContainer}>
-                        <Text style={EmojiPickerStyles.titleText}>
-                            {props.emojiPickerTitle}
-                        </Text>
-                    </View>
-                    <View style={EmojiPickerStyles.buttonContainer}>
-                        <Button color={"#007AFF"} title={"Dismiss"} onPress={() => { setShowEmojiPicker(false) }} />
-                    </View>
-                </View>
-                <View style={EmojiPickerStyles.emojiPicker}>
-                    <EmojiSelector
-                        showSearchBar={false}
-                        columns={9}
-                        onEmojiSelected={(emoji: string) => {
-                            setEmojiPicked(emoji)
-                            setShowEmojiPicker(false);
-                            props.onEmojiPicked(emoji)
-                        }} />
-                </View>
-            </SafeAreaView>
-        </Modal >
-    ) : (
-            <View style={EmojiPickerStyles.emojiContainer}>
-                <TouchableOpacity onPress={() => setShowEmojiPicker(true)}>
-                    <Text style={EmojiPickerStyles.emoji}>{emojiPicked}</Text>
-                </TouchableOpacity>
+  return showEmojiPicker ? (
+    <Modal visible={showEmojiPicker}>
+      <SafeAreaView style={EmojiPickerStyles.modalContainer}>
+        <View style={EmojiPickerStyles.headerContainer}>
+          <View style={EmojiPickerStyles.buttonContainer}>
+            <View style={EmojiPickerStyles.titleContainer}>
+              <Text style={EmojiPickerStyles.titleText}>
+                {emojiPickerTitle}
+              </Text>
             </View>
-        );
-}
+            <View style={EmojiPickerStyles.buttonContainer}>
+              <Button color='#007AFF' title='Dismiss' onPress={() => { setShowEmojiPicker(false); }} />
+            </View>
+          </View>
+          <View style={EmojiPickerStyles.emojiPicker}>
+            <EmojiSelector
+              showSearchBar={false}
+              columns={9}
+              onEmojiSelected={(emoji: string) => {
+                setEmojiPicked(emoji);
+                setShowEmojiPicker(false);
+                props.onEmojiPicked(emoji);
+              }}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    </Modal>
+  ) : (
+    <View style={EmojiPickerStyles.emojiContainer}>
+      <TouchableOpacity onPress={() => setShowEmojiPicker(true)}>
+        <Text style={EmojiPickerStyles.emoji}>{emojiPicked}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+EmojiPicker.defaultProps = {
+  defaultEmoji: DEFAULT_EMOJI,
+};
 
 export default EmojiPicker;
--- mobile/components/emojipicker/EmojiPickerStyles.tsx
+++ mobile/components/emojipicker/EmojiPickerStyles.tsx
@@ -1,43 +1,45 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
-export const EmojiPickerStyles = StyleSheet.create({
-    emoji: {
-        fontSize: 40,
-        alignItems: "center",
-        justifyContent: "center",
-        width: 45,
-    },
-    modalContainer: {
-        flex: 1,
-        display: "flex",
-        flexDirection: "column",
-    },
-    headerContainer: {
-        flex: .05,
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-    },
-    titleContainer: {
-        flex: 1
-    },
-    titleText: {
-        fontSize: 20,
-        fontFamily: "Ubuntu_700Bold",
-    },
-    buttonContainer: {
-        flex: .6,
-    },
-    emojiPicker: {
-        flex: .95,
-        marginTop: "5%"
-    },
-    emojiContainer: {
-        height: 55,
-        width: 55,
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: 5,
-        backgroundColor: "#E7EAF6"
-    }
-})
+const EmojiPickerStyles = StyleSheet.create({
+  emoji: {
+    fontSize: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 45,
+  },
+  modalContainer: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  headerContainer: {
+    flex: 0.05,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  titleContainer: {
+    flex: 1,
+  },
+  titleText: {
+    fontSize: 20,
+    fontFamily: 'Ubuntu_700Bold',
+  },
+  buttonContainer: {
+    flex: 0.6,
+  },
+  emojiPicker: {
+    flex: 0.95,
+    marginTop: '5%',
+  },
+  emojiContainer: {
+    height: 55,
+    width: 55,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    backgroundColor: '#E7EAF6',
+  },
+});
+
+export default EmojiPickerStyles;
