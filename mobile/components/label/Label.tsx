e25e7b5f8943082dbdd40271d6250045170ac009
Cynthia Wang <cynthiawang@berkeley.edu>
2020-09-13 15:00:08 +0800

    label linting

--- mobile/components/label/Label.tsx
+++ mobile/components/label/Label.tsx
@@ -1,27 +1,28 @@
-import React from "react"
-import { View, Text } from "react-native"
-import { TextStyles } from "../../TextStyles"
-
+import React from 'react';
+import { View, Text } from 'react-native';
+import { TextStyles } from '../../TextStyles';
 
 type LabelProps = {
-    labelText: string,
-    style?: object,
-    size?: "normal" | "small"
-}
+  labelText: string;
+  style?: object;
+  size?: 'normal' | 'small';
+};
 
 const Label = (props: LabelProps) => {
-    const size = props.size || "normal"
-    const textStyle = size == "normal" ? TextStyles.paragraph : TextStyles.secondary
-    return (
-        <View>
-            <Text style={[
-                textStyle,
-                props.style,
-            ]}>
-                {props.labelText}
-            </Text>
-        </View>
-    )
-}
+  const { size, style, labelText } = props;
+  const textStyle = size === 'normal' ? TextStyles.paragraph : TextStyles.secondary;
+  return (
+    <View>
+      <Text style={[textStyle, style]}>
+        {labelText}
+      </Text>
+    </View>
+  );
+};
 
-export default Label
+Label.defaultProps = {
+  style: {},
+  size: 'normal',
+};
+
+export default Label;
