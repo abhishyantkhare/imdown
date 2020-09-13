4e2c89844321af1f06a01c4389ec1eefc771d52e
Cynthia Wang <cynthiawang@berkeley.edu>
2020-09-13 15:14:47 +0800

    old datetimeinput linting

--- mobile/components/date_time_input/date_time_input.tsx
+++ mobile/components/date_time_input/date_time_input.tsx
@@ -1,23 +1,34 @@
-import React, { useState } from "react";
-import { Button, Modal, Platform, Text, TouchableOpacity, View } from "react-native";
-import DateTimePicker, { AndroidEvent } from "@react-native-community/datetimepicker";
-import { DateTimeInputStyles } from "./date_time_input_styles";
-import moment from "moment";
+/* TO BE DEPRECATED */
+
+import React, { useState } from 'react';
+import {
+  Button,
+  Modal,
+  Platform,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import DateTimePicker, { AndroidEvent } from '@react-native-community/datetimepicker';
+import moment from 'moment';
+
+import DateTimeInputStyles from './date_time_input_styles';
 
 // This can be extended for formatting, styling, and more.
 export interface DateTimeInputProps {
-  onChange: (dateTime: Date) => void;
+  onChange: (dateTime: Date) => void; // eslint-disable-line no-unused-vars
   // Initial value of the selected datetime. If not provided, the display will be TBD.
   initialValue?: Date;
   // Initial value of the selector. Defaults to the field value or the present time if necessary.
-  // TODO: this prop is not used at the moment, but it can be used or maybe converted to something stateful if we want
-  //       to initialize the end date selector using the value of the start date.
+  // TODO: this prop is not used at the moment, but it can be used or maybe converted to
+  // something stateful if we want to initialize the end date selector using the value
+  // of the start date.
   suggestedSelectorValue?: Date;
 }
 
 /**
- * A cross-platform date and time selector. iOS uses a date and time scroll wheel whereas Android uses a calendar
- * selector followed by a clock selector.
+ * A cross-platform date and time selector. iOS uses a date and time scroll wheel
+ * whereas Android uses a calendar selector followed by a clock selector.
  */
 const DateTimeInput = (dateTimeInputProps: DateTimeInputProps) => {
   // iOS uses a combined datetime selector while Android uses separate date and time popups.
@@ -25,24 +36,26 @@ const DateTimeInput = (dateTimeInputProps: DateTimeInputProps) => {
   const [mode, setMode] = useState<'datetime' | 'date' | 'time'>(defaultMode);
   // State of the input selector.
   // TODO: Pick a better fallback time.
-  const defaultSelectorValue = dateTimeInputProps.initialValue || dateTimeInputProps.suggestedSelectorValue || new Date();
+  const defaultSelectorValue = dateTimeInputProps.initialValue
+    || dateTimeInputProps.suggestedSelectorValue
+    || new Date();
   const [selectorDateTime, setSelectorDateTime] = useState<Date>(defaultSelectorValue);
   // State of the input field.
   const [dateTime, setDateTime] = useState<Date | undefined>(dateTimeInputProps.initialValue);
   const [show, setShow] = useState(false);
-
-  // Close the selector, set the field value to the selector value, and trigger the callback.
-  const submitDateTime = (selectedValue: Date) => {
-    close();
-    setDateTime(selectedValue);
-    dateTimeInputProps.onChange(selectedValue);
-  };
 
   // Close the selector without updating the field value.
   const close = () => {
     // (Android) Ensure the popup is hidden before updating the mode.
     setShow(false);
     setMode(defaultMode);
+  };
+
+  // Close the selector, set the field value to the selector value, and trigger the callback.
+  const submitDateTime = (selectedValue: Date) => {
+    close();
+    setDateTime(selectedValue);
+    dateTimeInputProps.onChange(selectedValue);
   };
 
   const onChange = (_event: AndroidEvent, selectedValue?: Date) => {
@@ -69,32 +82,32 @@ const DateTimeInput = (dateTimeInputProps: DateTimeInputProps) => {
     }
   };
 
-  const formatDate = () => {
-    return dateTime
-      ? `🗓 ${moment(dateTime).format("llll").toLocaleString()}`
-      : "TBD";
-  };
+  const formatDate = () => (
+    dateTime
+      ? `🗓 ${moment(dateTime).format('llll').toLocaleString()}`
+      : 'TBD'
+  );
 
-  const renderDateTimePicker = () => {
+  const renderDateTimePicker = () => (
     // (iOS) Render a Modal and button. This does not work well with dark mode.
     // (Android) The DateTimePicker appears as a popup dialog.
-    return Platform.OS === 'ios'
+    Platform.OS === 'ios'
       ? (
         <Modal>
-          <View style ={{marginTop: 300}}>
+          <View style={{ marginTop: 300 }}>
             <DateTimePicker value={selectorDateTime} mode={mode} onChange={onChange} />
-            <Button title="Cancel" onPress={close} />
-            <Button title="Set Date" onPress={() => submitDateTime(selectorDateTime)} />
+            <Button title='Cancel' onPress={close} />
+            <Button title='Set Date' onPress={() => submitDateTime(selectorDateTime)} />
           </View>
         </Modal>
       )
-      : <DateTimePicker value={selectorDateTime} mode={mode} onChange={onChange} />;
-  };
+      : <DateTimePicker value={selectorDateTime} mode={mode} onChange={onChange} />
+  );
 
   return (
     <View>
       <TouchableOpacity onPress={() => setShow(true)}>
-        <Text style={DateTimeInputStyles.time_text}>{formatDate()}</Text>
+        <Text style={DateTimeInputStyles.timeText}>{formatDate()}</Text>
       </TouchableOpacity>
       {show && renderDateTimePicker()}
     </View>
--- mobile/components/date_time_input/date_time_input_styles.tsx
+++ mobile/components/date_time_input/date_time_input_styles.tsx
@@ -1,11 +1,15 @@
-import { StyleSheet } from "react-native";
-export const DateTimeInputStyles = StyleSheet.create({
-  time_text: {
-    color: "grey",
-    fontFamily: "Ubuntu_400Regular",
+/* TO BE DEPRECATED */
+
+import { StyleSheet } from 'react-native';
+
+const DateTimeInputStyles = StyleSheet.create({
+  timeText: {
+    color: 'grey',
+    fontFamily: 'Ubuntu_400Regular',
     fontSize: 15,
     paddingLeft: 10,
     paddingVertical: 15,
   },
 });
 
+export default DateTimeInputStyles;
