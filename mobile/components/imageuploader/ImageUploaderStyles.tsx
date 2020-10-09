c7ae0f3da04e98818aca9e46ab144f3a11d994b2
c0dyzeng <codyzeng@berkeley.edu>
2020-10-08 21:24:48 -0700

    Fix padding issues for blur modal

--- mobile/components/imageuploader/ImageUploaderStyles.tsx
+++ mobile/components/imageuploader/ImageUploaderStyles.tsx
@@ -1,13 +1,18 @@
 import { StyleSheet } from 'react-native';
 
 const ImageUploaderStyles = StyleSheet.create({
+  photoFromCameraButton: {
+    width: '90%',
+  },
   chooseFromGalleryButton: {
     marginBottom: '15%',
     marginTop: '3%',
+    width: '90%',
   },
   deleteImageButton: {
     backgroundColor: '#FC6E5E',
     marginBottom: '10%',
+    width: '90%',
   },
 });
 
